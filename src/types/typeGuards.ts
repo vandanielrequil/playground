86aad4b011faf566fccb9dd5ea2a5d416fa16f4d
DanilSukhomlinov <dsukhomlinov@ironbricks.dev>
2024-09-12 16:47:03 +0400

    typeGuards

--- src/types/typeGuards.ts
+++ src/types/typeGuards.ts
@@ -1,1 +1,4 @@
-const isKeyOf = <T extends object>(key: any, obj: T): key is keyof T => key in obj;
+export const isKeyOf = <T extends object>(key: any, obj: T): key is keyof T => key in obj;
+
+export const isValueInUnionArray = <T extends string>(value: string, unionArray: readonly T[])
+  : value is T => unionArray.includes(value as T);
