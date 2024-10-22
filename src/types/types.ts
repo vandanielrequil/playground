63af07d5a417990f27fdd27633e2004827af1883
DanilSukhomlinov <dsukhomlinov@ironbricks.dev>
2024-10-22 13:29:20 +0400

    JSONValue

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,3 @@
 type Tuple<T, N extends number, R extends unknown[] = []> = R["length"] extends N ? R : Tuple<T, N, [T, ...R]>;
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
