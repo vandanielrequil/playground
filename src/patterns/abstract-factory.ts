e43277c655fb407c3a090227944c61fddcd01b58
DanilSukhomlinov <dsukhomlinov@ironbricks.dev>
2025-01-20 21:17:04 +0400

    abstract-factory

---
+++ src/patterns/abstract-factory.ts
@@ -0,0 +1,68 @@
+import { Maze, Door, Room, Wall, Direction } from "./template-maze";
+
+export class MazeFactory {
+    makeMaze(): Maze {
+        return new Maze();
+    }
+    makeWall(): Wall {
+        return new Wall();
+    }
+    makeRoom(i: number): Room {
+        return new Room(i);
+    }
+    makeDoor(r1: Room, r2: Room): Door {
+        return new Door(r1, r2);
+    }
+};
+
+export class MazeGame {
+    public createMaze(factory: MazeFactory): Maze {
+        const aMaze = factory.makeMaze();
+        const r1 = factory.makeRoom(1);
+        const r2 = factory.makeRoom(2);
+        const aDoor = factory.makeDoor(r1,r2);
+
+        aMaze.addRoom(r1);
+        aMaze.addRoom(r2);
+
+        r1.setSide( Direction.North, factory.makeWall());
+        r1.setSide( Direction.East, aDoor);
+        r1.setSide( Direction.South, factory.makeWall());
+        r1.setSide( Direction.West, factory.makeWall());
+
+        r2.setSide( Direction.North, factory.makeWall());
+        r2.setSide( Direction.East, factory.makeWall());
+        r2.setSide( Direction.South, factory.makeWall());
+        r2.setSide( Direction.West, aDoor);
+
+        return aMaze;
+    }
+}
+
+type Spell = string;
+class EnchantedRoom extends Room {
+    constructor(roomN: number, castSpell: Spell) {
+        super(roomN);
+    }
+};
+class DoorNeedingSpell extends Door {};
+export class EnchantedMazeFactory extends MazeFactory {
+    makeRoom(n: number): Room { return new EnchantedRoom(n, this.castSpell()); }
+    makeDoor(r1: Room, r2: Room): Door { return new DoorNeedingSpell(r1, r2); }
+
+    protected castSpell(): Spell { return 'avadacedavra' }
+};
+
+
+class RoomWithABomb extends Room {};
+class BombedWall extends Wall {};
+export class BombedMazeFactory extends MazeFactory {
+    makeWall(): Wall { return new BombedWall(); }
+    makeRoom(n: number): Room { return new RoomWithABomb(n); }
+};
+
+const maze = new MazeGame();
+
+const enchantedMaze = maze.createMaze(new EnchantedMazeFactory());
+
+const bombMaze = maze.createMaze(new BombedMazeFactory());
---
+++ src/patterns/template-maze.ts
@@ -0,0 +1,40 @@
+export enum Direction {North, South, East, West};
+
+abstract class MapSite {
+    public abstract enter(): void;
+}
+
+export class Room implements MapSite {
+    private _sides: MapSite[] = Array(4).fill(null);
+    private _roomNumber: number;
+
+    constructor(roomN: number) { this._roomNumber = roomN; }
+
+    public getSide(d: Direction): MapSite { return this._sides[d]; }    
+    public setSide(d: Direction, m: MapSite): void { this._sides[d] = m; }
+    public enter(): void {}
+}
+
+export class Door implements MapSite {
+    private _room1: Room;
+    private _room2: Room;
+    private _isOpen: boolean;
+
+    constructor(room1: Room, room2: Room) {
+        this._room1 = room1;
+        this._room2 = room2;
+    }
+
+    public enter(): void {}
+    public otherSideRoom(room: Room): Room { return room }
+}
+
+export class Wall implements MapSite {
+    public enter(): void {}
+}
+
+export class Maze {
+    public addRoom(room: Room): void {}
+
+    public roomNo(roomN: number): Room { return {} as Room }
+}
